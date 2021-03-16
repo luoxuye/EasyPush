f1fb95f12b6ddc5f625a9e949d7e33a29e02271a
XUYE LUO <42453877+luoxuye@users.noreply.github.com>
2021-03-15 18:26:07 -0600

    Create beijingtime.py

---
+++ beijingtime.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+
+
+utc_now = datetime.utcnow()
+bj_time = "\n北京时间\n" + str((utc_now + timedelta(hours=8)).strftime("%Y年%m月%d日%H时%M分%S秒"))
+
+
+
+print(bj_time)
+ 

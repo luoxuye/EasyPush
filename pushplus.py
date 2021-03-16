7e446c53ff10969d9ed5a72bb4f0d93052b5754e
XUYE LUO <42453877+luoxuye@users.noreply.github.com>
2021-03-15 19:53:06 -0600

    Update pushplus.py

--- pushplus.py
+++ pushplus.py
@@ -13,7 +13,6 @@ from datetime import datetime, timedelta
 utc_now = datetime.utcnow()
 bj_time = "\n北京时间\n" + str((utc_now + timedelta(hours=8)).strftime("%Y年%m月%d日%H时%M分%S秒"))
 
-print(bj_time)
 
 #PPTOKEN = ""
 PPTOKEN = os.environ["PPTOKEN"] #本地运行时直接填具体"参数".
@@ -32,9 +31,7 @@ if PPTOKEN != "":
     pushplus()
 
 print("PPTOKEN = " + PPTOKEN)
-
-
-
+print(bj_time)
  
  
 

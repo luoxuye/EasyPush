7c34aef2e6b2b3361dc753e0c031d20775fd07c6
XUYE LUO <42453877+luoxuye@users.noreply.github.com>
2021-03-15 20:49:55 -0600

    Update wecomapp.py

--- wecomapp.py
+++ wecomapp.py
@@ -10,24 +10,13 @@ WWID = os.environ["WWID"] #本地运行时直接填具体"参数".
 WWAPPSECRET = os.environ["WWAPPSECRET"] #本地运行时直接填具体"参数".
 WWAPPID = os.environ["WWAPPID"] #本地运行时直接填具体"参数".
 WWUSERID = os.environ["WWUSERID"] #本地运行时直接填具体"参数".选填
+
 if WWUSERID == "":
     WWUSERID = "@all"
  
 #北京时间
 utc_now = datetime.utcnow()
 bj_time = "  \n北京时间\n  " + str((utc_now + timedelta(hours=8)).strftime("%Y年%m月%d日%H时%M分%S秒"))
-
-
-
-
-
-def access_token():
-    url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?" + "corpid=" + WWID + "&corpsecret=" + WWAPPSECRET
-    response = requests.get(url).json()
-    access_token = response['access_token']
-    return access_token
-  
-
 
 def wecom():
     pushdata = {
@@ -45,11 +34,14 @@ def wecom():
       "duplicate_check_interval": 1800
     }
     
-    
     pushdata = json.dumps(pushdata)
-  
-    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + access_token()
-    res = requests.post(url=url, data=pushdata)
+    #get access_token
+    geturl = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={WWID}&corpsecret={WWAPPSECRET}"
+    response = requests.get(geturl).json()
+    access_token = response["access_token"]
+    #push
+    posturl = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}"
+    res = requests.post(url=posturl, data=pushdata)
     print(res.text)
   
 if WWID != "" and WWAPPSECRET != "" and WWAPPID != "":
@@ -59,4 +51,3 @@ print("WWID = " + WWID )
 print("WWAPPSECRET = " + WWAPPSECRET )
 print("WWAPPID = " + WWAPPID )
 print(bj_time)
-

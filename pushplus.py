9adb2b134cecf7e039fa2358c74f0173380e5ba1
XUYE LUO <42453877+luoxuye@users.noreply.github.com>
2021-03-15 18:34:50 -0600

    Update pushplus.py

--- pushplus.py
+++ pushplus.py
@@ -1,16 +1,25 @@
-from datetime import datetime, timedelta
+# Luo
+# Time: 2021-03-15 18:32:17
+
+
+# 打开EasyPush/.github/workflows/EasyPush.yml或者自己的****/.github/workflows/***.yml文件
+# 在env: 添加下面一行。
+#         env:
+#           PPTOKEN: ${{ secrets.PPTOKEN }}
+import os
 import requests
+from datetime import datetime, timedelta
 
 utc_now = datetime.utcnow()
 bj_time = "\n北京时间\n" + str((utc_now + timedelta(hours=8)).strftime("%Y年%m月%d日%H时%M分%S秒"))
 
 print(bj_time)
 
-#token = ""
-token = ""
+#PPTOKEN = ""
+PPTOKEN = ""
 def pushplus(data):
-    if token != "":
-        return requests.post(f"http://www.pushplus.plus/send?token={token}", data=data)
+    if PPTOKEN != "":
+        return requests.post(f"http://www.pushplus.plus/send?token={PPTOKEN}", data=data)
     else:
         return False
 
